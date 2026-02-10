42beb61fe5e485196644914fe25bae5c404bf2ac
Xhanti <xhantilomzimbasadaka@gmail.com>
2026-02-10 11:08:47 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JAVA BANKING PROGRAM
+
+
+        // DISPLAY VARIABLES
+        Scanner scanner = new Scanner(System.in);
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        // DISPLAY MENU
+        while(isRunning){
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
+
+            // GET AND PROCESS USERS CHOICE
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch(choice){
+                case 1 -> showBalance(balance);
+                case 2 -> System.out.println("DEPOSIT");
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+        // showBalance()
+
+
+        // deposit()
+
+        // withdraw()
+        //Betty waz her
+        //EXIT MESSAGE
+        scanner.close();
+    }
+    static void showBalance(double balance){
+        System.out.printf("$%f, balance");
+    }
+}
