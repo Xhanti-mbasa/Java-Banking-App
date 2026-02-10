f48f364f4c1699aee661cfa9226b087aadf9b6ea
xhanti-mbasa <xhantilomzimbasadaka@gmail.com>
2026-02-10 12:38:21 +0200

    made the deposit feature

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
+import javax.sound.midi.Soundbank;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         // JAVA BANKING PROGRAM
 
 
         // DISPLAY VARIABLES
-        Scanner scanner = new Scanner(System.in);
         double balance = 0;
         boolean isRunning = true;
         int choice;
@@ -29,7 +33,7 @@ public class Main {
 
             switch(choice){
                 case 1 -> showBalance(balance);
-                case 2 -> System.out.println("DEPOSIT");
+                case 2 -> balance = balance + deposit();
                 case 3 -> System.out.println("WITHDRAW");
                 case 4 -> isRunning = false;
                 default -> System.out.println("INVALID CHOICE");
@@ -38,14 +42,30 @@ public class Main {
         // showBalance()
 
 
-        // deposit()
 
         // withdraw()
         //Betty waz her
         //EXIT MESSAGE
         scanner.close();
     }
+    // showBalance()
     static void showBalance(double balance){
-        System.out.printf("$%f", balance);
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance); //Limit the decimal places to 2 by using $.2%f (% is just a place holder.
     }
+    // deposit()
+    static double deposit(){
+        double amount;
+        System.out.print("Enter an amount to be deposited; 0");
+        amount = scanner.nextDouble();
+
+        if(amount < 0){
+            System.out.print("Amount can't be  negitave");
+            return 0;
+        }
+        else{
+            return amount;
+        }
+    }
+
 }
