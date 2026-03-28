fb9ea550f31d1319bbb6fb89450a15299db10ada
xhanti-mbasa <xhantilomzimbasadaka@gmail.com>
2026-03-28 13:39:37 +0200

    fixed the error from Speghetti code 00:42 issue.

--- Client.java
+++ Client.java
@@ -1,18 +1,37 @@
 import java.util.Scanner;
 
 public class Client {
+
     private Scanner scanner = new Scanner(System.in);
     private Bank myBank = new Bank();
 
     public void startMenu() {
-        case 2 -> {
-            System.out.print("Enter amount to deposit: ");
-            double amount = scanner.nextDouble();
+        boolean isRunning = true;
 
-            if (myBank.deposit(amount)) {
-                System.out.println("Deposit succesful!");
-            } else {
-                System.out.println("Error: Please enter a positive number.");
+        while (isRunning) {
+            System.out.println("1. Show Balance\n2. Deposit\n4. Exit");
+            System.out.print("Enter choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 2 -> {
+                    try {
+                        System.out.print("Enter amount to deposit: ");
+                        double amount = scanner.nextDouble();
+
+                        if (myBank.deposit(amount)) {
+                            System.out.println("Deposit successful!");
+                        } else {
+                            System.out.println("Error: Please enter a positive number.");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Invalid input! Please enter a number.");
+                        scanner.next();
+                    }
+                }
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice.");
             }
         }
     }
