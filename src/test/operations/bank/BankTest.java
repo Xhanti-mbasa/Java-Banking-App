2c0a83db460f8dd2d6205c26d49294649bab2402
xhmohcpt025 <xhmohcpt025@student.wethinkcode.co.za>
2026-07-09 08:42:01 +0200

    fix: add target/ to gitignore

---
+++ src/test/operations/bank/BankTest.java
@@ -0,0 +1,16 @@
+package operations.bank;
+
+public class BankTest {
+
+    @org.junit.Test
+    public void getBalance() {
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+    }
+
+    @org.junit.Test
+    public void deposit() {
+    }
+}
