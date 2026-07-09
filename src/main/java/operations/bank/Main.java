6b7b7805304cbd1966954034dde25bdf765ff265
xhmohcpt025 <xhmohcpt025@student.wethinkcode.co.za>
2026-07-09 09:29:22 +0200

    fix: add missing class declaration to Main.java

--- src/main/java/operations/bank/Main.java
+++ src/main/java/operations/bank/Main.java
@@ -1,10 +1,11 @@
+package operations.bank;
+
 import operations.client.Client;
 
-void main() {
-    Client client = new Client();
-
-    client.startMenu();
-
-    System.out.println("Thank you for using Xhanti's bank GOODBYE!");
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startMenu();
+        System.out.println("Thank you for using Xhanti's bank GOODBYE!");
+    }
 }
-
